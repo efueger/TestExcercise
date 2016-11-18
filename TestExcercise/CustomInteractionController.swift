95cd1ecd2154eb7f1e06469b650d446a3e50a952
Fatima Dairkee <fatemamhow@yahoo.com>
2016-11-18 00:39:58 +0000

    small changes related to formatting and gitattrinute

---
+++ TestExcercise/CustomInteractionController.swift
@@ -0,0 +1,13 @@
+//
+//  CustomInteractionController.swift
+//  TestExcercise
+//
+//  Created by Fatima mhowwala on 10/11/16.
+//  Copyright © 2016 ThoughtBeans. All rights reserved.
+//
+
+import UIKit
+
+class CustomInteractionController: UIPercentDrivenInteractiveTransition {
+
+}
